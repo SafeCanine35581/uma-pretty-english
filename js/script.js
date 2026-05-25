79f864e5e9bdf9e2b67a77e3db890bce9ad5a9b3
RosettaDev <105999813+SafeCanine35581@users.noreply.github.com>
2026-05-24 21:44:56 -0400

    background changes depending on UTC time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 var Mambo = new Audio('assets/mambo.mp3');
 
+function setBackground() {
+    const background = document.getElementById('menu_container');
+    const currentTime = new Date().getUTCHours();
+
+    let backgroundImage = '';
+
+    if (currentTime >= 5 && currentTime < 8) {
+        backgroundImage = '../img/bg/bg_morning.webp'; 
+    } else if (currentTime >= 8 && currentTime < 18) {
+        backgroundImage = '../img/bg/bg_day.webp'; 
+    } else if (currentTime >= 18 && currentTime < 21) {
+        backgroundImage = '../img/bg/bg_night.webp';
+    } else if (currentTime >= 21 || currentTime < 5) {
+        backgroundImage = '../img/bg/bg_off.webp';
+    } else {
+        backgroundImage = '../img/bg/bg_day.webp';
+    }
+    background.style.backgroundImage = `url(${backgroundImage})`;
+    console.log('Time: ' + currentTime + ' | Background: ' + backgroundImage);
+}
+
+setBackground();
+setInterval(setBackground, 60000);
+
 function playMambo() {
     Mambo.play();
 }
